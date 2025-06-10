23849b249473989e3f3bd32fe53e3fd5bcccb1c7
dmitryreaper <dimadimof81@gmail.com>
2025-06-10 08:43:59 +0300

    nix development environment

--- shell.nix
+++ shell.nix
@@ -4,6 +4,32 @@ pkgs.mkShell {
   buildInputs = [
     pkgs.haskellPackages.ghc
     pkgs.haskellPackages.cabal-install
-    pkgs.haskellPackages.gtk3
+    pkgs.gtk3              # Или pkgs.gtk4, если хочешь использовать GTK 4
+    pkgs.gobject-introspection
+    pkgs.haskellPackages.gi-gtk
+	pkgs.pkg-config
+	pkgs.pcre2
+	pkgs.expat
+	pkgs.xorg.libXdmcp
+	pkgs.mount
+	pkgs.util-linux.dev
+	pkgs.libselinux
+	pkgs.libsepol
+	pkgs.fribidi
+	pkgs.libthai
+    pkgs.libxml2
+	pkgs.libdatrie
+	pkgs.libsysprof-capture
+	pkgs.lerc
+	pkgs.glib
+
+#gtk3
+	pkgs.libxkbcommon
+	pkgs.libepoxy
+	pkgs.xorg.libXtst
   ];
+
+  shellHook = ''
+    echo "Добро пожаловать в окружение разработки gi-gtk!"
+  '';
 }
