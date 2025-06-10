febc07331ebb156fe812e02e9323f39b98c3f17e
dmitryreaper <dimadimof81@gmail.com>
2025-06-10 08:43:10 +0300

    simple gtk application

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,24 @@
-module Main where
+
+{-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  Gtk.init Nothing
+
+  win <- new Gtk.Window [ #title := "First app" ]
+
+  on win #destroy Gtk.mainQuit
+
+  button <- new Gtk.Button [ #label := "Нажми сюда" ]
+
+  on button #clicked (set button [ #sensitive := True,
+                                   #label := "Спасибо за нажатие" ])
+
+  #add win button
+
+  #showAll win
+
+  Gtk.main
