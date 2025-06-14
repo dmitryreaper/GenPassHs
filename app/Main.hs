07db147fd6699295e6e79191c6ca753505df8abb
dmitryreaper <dimadimof81@gmail.com>
2025-06-14 09:26:59 +0300

    add new widgets

--- app/Main.hs
+++ app/Main.hs
@@ -1,24 +1,55 @@
-
 {-# LANGUAGE OverloadedStrings, OverloadedLabels #-}
-
 import qualified GI.Gtk as Gtk
 import Data.GI.Base
+import Data.IORef
+import Control.Monad.IO.Class (liftIO)
 
 main :: IO ()
 main = do
+  -- Инициализация GTK
   Gtk.init Nothing
 
+  -- Создание окна
   win <- new Gtk.Window [ #title := "First app" ]
-
   on win #destroy Gtk.mainQuit
 
+  -- Создание контейнера (вертикального Box)
+  box <- new Gtk.Box [ #orientation := Gtk.OrientationVertical, #spacing := 5 ]
+
+  labelHello <- new Gtk.Label [ #label := "Привет от Haskell!" ]
+
+
+  -- Первая кнопка
   button <- new Gtk.Button [ #label := "Нажми сюда" ]
+  on button #clicked $ set button [ #label := "Спасибо за нажатие" ]
 
-  on button #clicked (set button [ #sensitive := True,
-                                   #label := "Спасибо за нажатие" ])
+  -- Вторая кнопка
+  buttona <- new Gtk.Button [ #label := "Button" ]
+  on buttona #clicked $ set buttona [ #label := "Спасибо за нажатие" ]
 
-  #add win button
+  -- === Метка 2: Счётчик нажатий ===
+  clickCountRef <- newIORef (0 :: Int)
+  labelCounter <- new Gtk.Label [ #label := "Количество нажатий: 0" ]
 
-  #showAll win
+  let updateCounter = do
+        modifyIORef' clickCountRef (+1)
+        count <- readIORef clickCountRef
+        set labelCounter [ #label := "Количество нажатий: "]
+  
+  on button #clicked updateCounter
+  on buttona #clicked updateCounter
 
+  -- Добавляем кнопки в контейнер
+  #add box labelCounter
+  #add box labelHello  
+  #add box button
+  #add box buttona
+
+  -- Добавляем контейнер в окно
+  #add win box
+
+  -- Отображаем все элементы
+  Gtk.widgetShowAll win
+
+  -- Запуск главного цикла GTK
   Gtk.main
