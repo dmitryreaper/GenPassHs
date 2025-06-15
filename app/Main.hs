343524954e01870e3060741351f756097ddfe6b1
dmitryreaper <dimadimof81@gmail.com>
2025-06-15 11:32:51 +0300

    gen password

--- app/Main.hs
+++ app/Main.hs
@@ -1,54 +1,96 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLabels, ImplicitParams,
-PackageImports #-}
-
-import Control.Monad (void)
-import System.Environment (getArgs, getProgName)
-
-import Data.Int (Int32)
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE ImplicitParams #-}
 
 import qualified GI.Gtk as Gtk
--- We import Application explicitly to test imports from the
--- backwards-compatibility wrapper gi-gtk. In general it would be
--- simpler to use Gtk.Application directly.
-import "gi-gtk" GI.Gtk.Objects.Application (Application(..))
+import qualified GI.Adw as Adw
 import Data.GI.Base
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Char (isDigit, isLower, isUpper)
+import System.Random (randomRIO)
+import Control.Monad (replicateM, void)
+import System.Environment (getArgs, getProgName)
 
--- | An example of a signal callback accessing the ?self parameter
--- (that is, the object raising the callback). See
--- https://github.com/haskell-gi/haskell-gi/issues/346
--- for why this is necessary when dealing with even controllers in gtk4.
-pressedCB :: (?self :: Gtk.GestureClick) => Int32 -> Double -> Double -> IO ()
-pressedCB nPress x y = do
-    button <- #getCurrentButton ?self
-    putStrLn $ "Button pressed: " <> show nPress <> " "
-      <> show x <> " " <> show y <> " button: " <> show button
+charset :: String
+charset = ['a'..'z'] ++ ['A'..'Z'] ++ ['0'..'9'] ++ "!@#$%^&*_+-"
 
-activate :: Gtk.Application -> IO ()
+generatePassword :: Int -> IO Text
+generatePassword len = do
+  idxs <- replicateM len (randomRIO (0, length charset - 1))
+  return $ T.pack [charset !! i | i <- idxs]
+
+passwordStrength :: Text -> (Text, String)
+passwordStrength pwd
+  | len < 8   = ("Слабый", "red")
+  | len < 12  = ("Средний", "orange")
+  | hasUpper && hasLower && hasDigit && hasSpecial = ("Сильный", "green")
+  | otherwise = ("Средний", "orange")
+  where
+    len = T.length pwd
+    hasUpper = T.any isUpper pwd
+    hasLower = T.any isLower pwd
+    hasDigit = T.any isDigit pwd
+    hasSpecial = T.any (`elem` ("!@#$%^&*_+-" :: String)) pwd
+
+activate :: Adw.Application -> IO ()
 activate app = do
-  box <- new Gtk.Box [#orientation := Gtk.OrientationVertical]
+  content <- new Gtk.Box
+    [ #orientation := Gtk.OrientationVertical
+    , #spacing := 12
+    , #marginStart := 20
+    , #marginEnd := 20
+    , #marginTop := 20
+    , #marginBottom := 20
+    ]
 
-  adjustment <- new Gtk.Adjustment [#value := 50, #lower := 0, #upper := 100,
-                                    #stepIncrement := 1]
-  slider <- new Gtk.Scale[#adjustment := adjustment, #drawValue := True]
-  #append box slider
-  spinButton <- new Gtk.SpinButton [#adjustment := adjustment]
-  #append box spinButton
+  lenLabel <- new Gtk.Label [#label := "Длина пароля: 12"]
+  adjustment <- new Gtk.Adjustment [#value := 12, #lower := 8, #upper := 64, #stepIncrement := 1]
+  scale <- new Gtk.Scale [#orientation := Gtk.OrientationHorizontal, #adjustment := adjustment]
+  scale.setDigits 0
+  content.append lenLabel
+  content.append scale
 
-  controller <- new Gtk.GestureClick [After #pressed pressedCB]
-  #addController slider controller
+  result <- new Gtk.Entry [#editable := False, #placeholderText := "Здесь появится пароль"]
+  content.append result
 
-  window <- new Gtk.ApplicationWindow [#application := app,
-                                       #title := "Hello",
-                                       #child := box]
-  #show window
+  strengthLabel <- new Gtk.Label [#label := ""]
+  content.append strengthLabel
+
+  generateBtn <- new Gtk.Button [#label := "Сгенерировать 🔐"]
+  content.append generateBtn
+
+  on scale #valueChanged $ do
+    val <- round <$> scale.getValue
+    lenLabel.setLabel $ "Длина пароля: " <> T.pack (show val)
+
+  let updatePassword = do
+        len <- round <$> scale.getValue
+        pwd <- generatePassword len
+        result.setText pwd
+        let (strText, color) = passwordStrength pwd
+        strengthLabel.setMarkup $ "<span foreground=\"" <> T.pack color <> "\">" <> strText <> "</span>"
+
+  on generateBtn #clicked updatePassword
+
+  window <- new Adw.ApplicationWindow
+    [ #application := app
+    , #defaultWidth := 400
+    , #defaultHeight := 280
+    , #content := content
+    ]
+
+  window.setTitle (Just (T.pack "Генератор паролей"))
+
+  window.present
 
 main :: IO ()
 main = do
-  app <- new Application [#applicationId := "haskell-gi.Gtk4.test",
-                          On #activate (activate ?self)]
-
-  -- If the application does not need to parse command line arguments
-  -- just pass Nothing.
+  app <- new Adw.Application
+    [ #applicationId := "haskell.password.generator"
+    , On #activate (activate ?self)
+    ]
   args <- getArgs
   progName <- getProgName
-  void $ #run app (Just $ progName : args)
+  void $ app.run (Just (progName : args))
